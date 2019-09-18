1bf92a964bde7c13ff5cfb326b2b556ba0769022
Thiago Canete <thiago.canete@gmail.com>
2019-09-17 22:06:34 -0300

    criacao de mocks

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -2,41 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LivrariaApi.Mocks;
+using LivrariaApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LivrariaApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class LivroController : ControllerBase
     {
-        // GET api/values
+        // GET api/livro
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Livro>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var livros = new LivroMock().ListarLivros();
+
+            return livros;//new string[] { "livro1", "livro2" };
         }
 
-        // GET api/values/5
+        // GET api/livro/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "livro" + id;
         }
 
-        // POST api/values
+        // POST api/livro
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/livro/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/livro/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
